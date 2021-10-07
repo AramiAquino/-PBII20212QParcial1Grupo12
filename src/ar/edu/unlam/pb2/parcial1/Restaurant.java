ecb508dd92b6942d3ebc5613a63719e980339466
ManuelOyaregui <moyaregui@alumno.unlam.edu.ar>
2021-10-07 17:08:19 -0300

    Se modifico los metodos para calcular la deuda de las mesas

--- src/ar/edu/unlam/pb2/parcial1/Restaurant.java
+++ src/ar/edu/unlam/pb2/parcial1/Restaurant.java
@@ -17,6 +17,7 @@ public class Restaurant {
 		this.pedidos = new Pedido[10000];
 		this.menuComidas = new Comida[100];
 		this.menuBebidas = new Bebida[100];
+		this.totalDeuda = 0d;
 	}
 
 	public String getNombre() {
@@ -67,6 +68,19 @@ public class Restaurant {
 		this.menuBebidas = menuBebidas;
 	}
 
+	public Boolean agregarUnMozo(Mozo mozo) {
+		Boolean sePudoAgregar = false;
+
+		for (int i = 0; i < mozos.length; i++) {
+			if (mozos[i] == null) {
+				mozos[i] = mozo;
+				sePudoAgregar = true;
+				break;
+			}
+		}
+		return sePudoAgregar;
+	}
+	
 	public Boolean agregarUnaMesa(Mesa mesa) {
 		Boolean sePudoAgregar = false;
 
@@ -120,9 +134,34 @@ public class Restaurant {
 		return sePudoAgregar;
 	}
 
-	public Double calcularTotalDeuda(Pedido pedido) {
+	public Boolean agregarUnPedidoAPedidos(Pedido pedido) {
+		Boolean sePudoAgregar = false;
 		for (int i = 0; i < pedidos.length; i++) {
-			totalDeuda = (pedido.getBebida().getPrecio())+(pedido.getComida().getPrecio());
+			if (pedidos[i] == null) {
+				pedidos[i] = pedido;
+				sePudoAgregar = true;
+				break;
+			}
+		}
+		return sePudoAgregar;
+	}
+
+	public Double calcularDeudaDeUnaMesa(Integer nroMesa) {
+		Double deuda = 0d;
+		for (int i = 0; i < pedidos.length; i++) {
+			if (pedidos[i] != null && pedidos[i].getMesa().getNumero() == nroMesa) {
+				deuda = pedidos[i].getBebida().getPrecio() + pedidos[i].getComida().getPrecio();
+				break;
+			}
+		}
+		return deuda;
+	}
+
+	public Double calcularTotalDeudaTodasLasMesas() {
+		for (int i = 0; i < pedidos.length; i++) {
+			if (pedidos[i] != null) {
+				this.totalDeuda += pedidos[i].getBebida().getPrecio() + pedidos[i].getComida().getPrecio();
+			}
 		}
 		return totalDeuda;
 	}
